958f98e7b0de4a32e59864984bae813c8693d8b7
rohit workspace069 <rohitwork069@gmail.com>
2024-02-20 16:00:58 +0530

    solved some more

---
+++ questions_sol/uniqueno.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+ int unique1( int arr[], int n){
+    int xorsum=0;
+    for(int i=0; i<n; i++){
+        xorsum=xorsum^arr[i];
+    }
+    return xorsum;
+ }
+
+ int unique1( int arr[], int n){
+    int xorsum=0;
+    for(int i=0; i<n; i++){
+        xorsum=xorsum^arr[i];
+    }
+    return xorsum;
+ }
+
+int main() {
+      // call function
+
+    return 0;
+}
